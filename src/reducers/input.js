a36752d4d8a04a863019a1a3e8b6a09587e3e7f7
nishaya <nishaya@users.noreply.github.com>
2017-04-16 10:33:13 +0900

    impl. reducer

--- src/reducers/input.js
+++ src/reducers/input.js
@@ -1,5 +1,14 @@
 import createReducer from 'utils/create_reducer'
+import { INPUT_KEY_DOWN } from 'actions/action_types'
 
-const initialState = {}
+const initialState = {
+  pressedKeys: {},
+}
 export default createReducer(initialState, {
+  [INPUT_KEY_DOWN]: (state, action) => (
+    {
+      ...state,
+      pressedKeys: { ...state.pressedKeys, [action.payload.key]: true },
+    }
+  ),
 })
