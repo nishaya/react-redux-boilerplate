e454c00783c7e8cebcadd9eb2ebaf145a3a6a733
nishaya <nishaya@users.noreply.github.com>
2017-05-11 16:02:07 +0900

    add redbox

--- src/application.jsx
+++ src/application.jsx
@@ -3,17 +3,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import RedBox from 'redbox-react'
 import { buildStore } from 'utils/build_store'
 import IndexComponent from 'components/index'
 import _applicationStyle from './application.scss'
 
 const store = buildStore()
+const root = document.getElementById('app')
 
-render(
-  (<Provider store={store}>
-    <div>
-      <IndexComponent />
-    </div>
-  </Provider>),
-  document.getElementById('app'),
-)
+try {
+  render(
+    (<Provider store={store}>
+      <div>
+        <IndexComponent />
+      </div>
+    </Provider>),
+    root,
+  )
+} catch (e) {
+  render(<RedBox error={e} />, root)
+}
